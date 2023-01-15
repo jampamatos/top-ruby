4d7f74b335d35f2148ba8b9860ef804541067e39
João Paulo <jp.coutm@gmail.com>
2023-01-15 18:10:24 -0300

    Trying to implement #inorder

--- basic-ruby-projects/binary_search_tree/lib/tree.rb
+++ basic-ruby-projects/binary_search_tree/lib/tree.rb
@@ -26,26 +26,91 @@ class Tree
   end
 
   def insert(value, node = @root)
-    return Node.new(value) unless node
+    if node.nil?
+      @root = Node.new(value)
+      return
+    end
 
     if value < node.data
-      node.left_child = insert(value, node.left_child)
+      if node.left_child.nil?
+        node.left_child = Node.new(value)
+      else
+        insert(value, node.left_child)
+      end
     elsif value > node.data
-      node.right_child = insert(value, node.right_child)
-    elsif value == node.data
+      if node.right_child.nil?
+        node.right_child = Node.new(value)
+      else
+        insert(value, node.right_child)
+      end
+    else
       raise 'Value already exists in the tree'
     end
+  end
+
+  def delete(value, node = @root)
+    return nil if node.nil?
+
+    if value < node.data
+      node.left_child = delete(value, node.left_child)
+    elsif value > node.data
+      node.right_child = delete(value, node.right_child)
+    else
+      if node.left_child.nil?
+        temp = node.right_child
+        node = nil
+        return temp
+      elsif node.right_child.nil?
+        temp = node.left_child
+        node = nil
+        return temp
+      end
+      temp = find_min(node.right_child)
+      node.data = temp.data
+      node.right_child = delete(temp.data, node.right_child)
+    end
+    @root = temp if node == @root && node.data == value
+
     node
   end
 
-  def delete(value, nood = @root) end
+  def find_min(node)
+    current_node = node
+    while (current_node.left_child) do
+      current_node = current_node.left_child
+    end
+    current_node
+  end
+
+  def level_order
+    queue = [@root]
+    values = []
+
+    until queue.empty?
+      node = queue.shift
+      yield node.data if block_given?
+
+      queue << node.left_child if node.left_child
+      queue << node.right_child if node.right_child
+      values << node.data
+    end
+    block_given? ? nil : values
+  end
+
+  def inorder(node = @root, values = [])
+    inorder(node.left_child, values) if node.left_child
+    if block_given?
+      yield node.data
+    else
+      values << node.data
+    end
+    inorder(node.right_child, values) if node.right_child
+    return values unless block_given?
+  end
 end
 
-tree = Tree.new([1, 2, 3, 4])
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
 tree.pretty_print
-tree.insert(5)
-tree.pretty_print
-tree.insert(6)
-tree.pretty_print
-tree.insert(7)
-tree.pretty_print
+
+tree.inorder { |node_data| puts node_data }
+p tree.inorder
